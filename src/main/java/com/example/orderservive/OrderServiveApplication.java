bf6157018cd0e7419a585fa0a84de20e1539afd3
ashish_git <sharmaashish.565@gmail.com>
2021-05-30 18:20:26 +0530

    order Service added servelet

--- src/main/java/com/example/orderservive/OrderServiveApplication.java
+++ src/main/java/com/example/orderservive/OrderServiveApplication.java
@@ -1,18 +1,26 @@
 package com.example.orderservive;
 
-import io.reactivex.Flowable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class OrderServiveApplication {
+public class OrderServiveApplication extends SpringBootServletInitializer {
+
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+	return 	builder.sources(OrderServiveApplication.class);
+	//	return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiveApplication.class, args);
 
-		Flowable.just("Hello World!").subscribe(System.out::println);
 	}
-
 }
